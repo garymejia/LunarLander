f911a7e2a17b228e7d4e8e1b6c4ed4cb05192dd6
pocck <50064854+pocck@users.noreply.github.com>
2020-03-29 19:18:20 -0400

    Add files via upload

---
+++ LunarLander.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+
+/*
+ * This class holds the first layer to the game. It creates an instance of game and holds the event listeners
+ * for the keys the users might press
+ */
+
+namespace LunarLander
+{
+    public partial class LunarLander : Form
+    {
+        public LunarLander()
+        {
+            InitializeComponent();
+        }
+
+        //Creates an instance of the game class
+        Game game = new Game();
+        
+        /*
+         * KeyDown has to be registered in a hash set so that multiple keys can be read at once
+         * 
+         */
+        private void keyDown(object sender, KeyEventArgs e)
+        {
+            //Listens to escape key (Layer 1)
+            if (e.KeyData == Keys.Escape)
+            {
+                //Creates a new instance of the game class which will restart the game and create new terrain
+                game = new Game();
+            }
+            //Registers keys to hash set in game class
+            game.KeyDown(e);
+        }
+
+        /*
+         * The key up method will remove the keys from the hash set
+         */
+        private void keyUp(object sender, KeyEventArgs e)
+        {
+            game.KeyUp(e);
+        }
+        /*
+         * The timer method will execute repeatedly 
+         * This update will update the sprite and current status of the game (Pause or not paused)
+         */
+        private void timer(object sender, EventArgs e)
+        {
+            //updates the sprites location and rotation, current status of the game.....
+            game.Update(new TimeSpan(updateTimer.Interval * 10000));
+            //Displays and applys all the updated information to the screen
+            gameScreen.Image = game.Draw();
+        }
+        
+
+
+        /*
+        HashSet<GameKeys> keys = new HashSet<GameKeys>();
+        private void keyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    keys.Add(GameKeys.Left);
+                    break;
+                case Keys.Right:
+                    keys.Add(GameKeys.Right);
+                    break;
+                case Keys.Up:
+                    keys.Add(GameKeys.Up);
+                    break; 
+                case Keys.Down:
+                    keys.Add(GameKeys.Down);
+                    break;
+                case Keys.Space:
+                    keys.Add(GameKeys.Space);
+                    break;
+            }
+        }
+
+        private void keyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    keys.Remove(GameKeys.Left);
+                    break;
+                case Keys.Right:
+                    keys.Remove(GameKeys.Right);
+                    break;
+                case Keys.Up:
+                    keys.Remove(GameKeys.Up);
+                    break;
+                case Keys.Down:
+                    keys.Remove(GameKeys.Down);
+                    break;
+                case Keys.Space:
+                    keys.Remove(GameKeys.Space);
+                    break;
+            }
+
+        }
+        private void timer(object sender, EventArgs e)
+        {
+            updateImage(new TimeSpan(updateTimer.Interval * 10000));
+        }
+
+        int X = 200;
+        int Y = 100;
+        Bitmap bmp = new Bitmap(640, 480);
+        private static String imageName = "tile002";
+        public Bitmap sprite = (Bitmap)Properties.Resources.ResourceManager.GetObject(imageName);
+        int z = 0;
+
+        private void updateImage(TimeSpan ts)
+        {
+            Y += 3;
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.Black);
+
+                Matrix m = new Matrix();
+
+                if (keys.Contains(GameKeys.Left) && (keys.Contains(GameKeys.Up)))
+                {
+                    X -= 5;
+                    Y -= 5;
+                    z+=20;
+                    m.RotateAt((z), new PointF(X, Y));
+                    Console.WriteLine("HERE");
+                }
+                else if (keys.Contains(GameKeys.Right) && (keys.Contains(GameKeys.Up)))
+                {
+                    X += 5;
+                    Y -= 5;
+                }
+                else if (keys.Contains(GameKeys.Up))
+                {
+                    Y -= 5;
+                }
+                else if (keys.Contains(GameKeys.Down))
+                {
+                    Y += 5;
+                }
+                else if (keys.Contains(GameKeys.Left))
+                {
+                    X -= 5;
+                }
+                else if (keys.Contains(GameKeys.Right))
+                {
+                    X += 5;
+                }
+                
+                g.Transform = m;
+                g.DrawImage(sprite, new PointF(X, Y));
+                m.Reset();
+                g.Transform = m;
+
+            }
+            pictureBox1.Image = bmp;
+        }*/
+    }
+}
---
+++ Terrain.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LunarLander
+{
+    class Terrain
+    {
+        //Constructor
+        public Terrain() {
+            points = new List<PointF>();
+        }
+
+
+        public int TotalHeight = 100;
+        public int ScreenHeight = 480;
+        public int midHeight = 50;
+        public int Width = 800;
+        const int landingStripWidth = 30;
+        public List<PointF> points;
+
+        public void GenerateTerrain()
+        {
+            Random rnd = new Random();
+
+            float tempHeight = 0;
+            //currently has 11 points
+            //our terrain is left list of connected dots in random height < 1/2 of screen
+            for (int w = 0; w < Width; w += rnd.Next(1, 10))
+            {
+
+                double amp = .2;
+                double period = 7;
+                int currentWave = 0;
+                /*
+                switch (currentWave)
+                {
+                    case 0:
+                        amp = .2;
+                        period = 7;
+                        break;
+                    case 1:
+
+                        break;
+                    case 2:
+                        break;
+
+                }*/
+                
+                tempHeight = Convert.ToSingle(amp * Math.Sin(period * w)+1);
+                Console.WriteLine(tempHeight);
+                tempHeight += rnd.Next(0, 40);
+                points.Add(new PointF(w, tempHeight));
+            }
+
+            int landingsCount = rnd.Next(3) + 2;
+            for (int i = 0; i < landingsCount; i++)
+            //insert landings 
+            {
+                int idx = rnd.Next(points.Count);
+                points.Insert(idx + 1, (new PointF(points[idx].X + landingStripWidth, points[idx].Y)));
+            }
+            points.Add(new PointF(Width, rnd.Next(TotalHeight)));
+            /*
+            //"normalize terrain" - lower it to the lowest point
+            //find lowest point
+            float lowest = points.Min(p => p.Y);
+*/
+            //decrease each
+            for (int i = 0; i < points.Count; i++)
+            {
+                points[i] = new PointF(points[i].X, ScreenHeight - (points[i].Y));//- lowest));
+            }
+
+        }
+    }
+}
